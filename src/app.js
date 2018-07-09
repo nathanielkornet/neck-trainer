ec3833cdcc2ed054d668980a30e4b74fe15d9e4b
Nathaniel Kornet <nathanielkornet@gmail.com>
2018-07-08 19:42:53 -0700

    a few improvements

--- src/app.js
+++ src/app.js
@@ -7,21 +7,30 @@ export default class App extends Component {
     super()
 
     this.state = {
-      isRunning: false
+      isRunning: false,
+      stringNames: ['E1', 'A1', 'D2', 'G2'],
+      startFret: 1,
+      endFret: 5,
+      interval: 2000
     }
 
     this.toggleIsRunning = () => this.setState({isRunning: !this.state.isRunning})
+
+    this.setAppParams = (stringNames, startFret, endFret, interval) =>
+      this.setState({stringNames, startFret, endFret, interval})
   }
   render () {
+    const { isRunning } = this.state
     return <div>
-      <h1>Learn you a neck</h1>
-      <h2>the shit {this.state.isRunning ? 'is' : 'is not'} running</h2>
+      <h1>Learn that neck</h1>
+      <h2>the shit is {isRunning ? '' : ' not'} running</h2>
       {this.state.isRunning
-        ? <Trainer stringNames={['E1']} startFret={0} endFret={24} interval={188}/>
-        : <Form />
+        ? <Trainer {...this.state} />
+        : <Form setAppParams={this.setAppParams} {...this.state} />
       }
       <button onClick={this.toggleIsRunning}>
-        flip the shit
+        {isRunning ? 'stop' : 'start'}
+        {' the shit'}
       </button>
     </div>
   }
